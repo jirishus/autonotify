8a0aed17b2c6546ea5d5bb7dcea980330f536099
jirishus <jirishus@yahoo.com>
2016-10-09 11:34:31 -0700

    test updates

--- routes/githandle.js
+++ routes/githandle.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cmd = require('node-cmd');
 
 router.post('/', function(req, res, next) {
 
+  cmd.get('pwd', function(data) {
+    console.log('current dir',data);
+  });
 
-  console.log('github request');
-  console.log(req.body);
+  cmd.run('cd /usr/share/nginx/html && git pull origin master');
 
   res.send('do git autoupdate');
 
