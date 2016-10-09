3dcc48190d4f2bb138fcd97dfe86e0925e962971
jirishus <jirishus@yahoo.com>
2016-10-09 09:25:21 -0700

    basic githandle

--- routes/githandle.js
+++ routes/githandle.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/githandle', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+
+
+  console.log('github request');
+  console.log(req.data);
 
   res.send('do git autoupdate');
 
